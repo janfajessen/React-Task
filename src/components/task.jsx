1febb1803344a764ed3a78f45f359b1f4d6b267b
janfajessen <janfajessen@gmail.com>
2019-04-15 10:02:55 +0200

    Task done with problems

--- src/components/task.jsx
+++ src/components/task.jsx
@@ -1,13 +1,75 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './task.scss';
 
-class Task extends Component {
+
+class Task extends React.Component {
+    state = {
+        newText: this.props.data.text,
+
+    }
+
+    // editing: false,
+    // newText: this.props.data.text
+    // toggleEditMode = () => {
+    //     this.setState({ editing: !this.state.editing });
+    // }
+
+    handleChange = (ev) => {
+        this.setState({ newText: ev.target.value });
+    }
+    // this.setState({ newText: ev.target.value }, { newTask: ev.target.createAt });
+
+    isEnter = ev => {
+        if (ev.keyCode === 13) {
+            this.edit(ev)
+        }
+    }
+    // date = (ev) => {
+    //     if (ev.onClick) {
+    //         this.setState({ newTask: ev.target.createAt })
+    //     }
+    // }
+
+    edit = (ev) => {
+        let editedTask = {
+            ...this.props.data,   //agregar un nuevo elemento a uno existente
+            text: this.state.newText
+        }
+        this.props.updateTask(editedTask);
+        ev.target.blur();
+    }
+
+    markAsCompleted = () => {
+        let editedTask = {
+            ...this.props.data,
+            completed: !this.props.data.completed,
+        };
+        this.props.updateTask(editedTask);
+    }
+
     render() {
         return (
-            <div className="task">{this.props.text}</div>
-        );
-    }
+            <div className={'task ' + (this.props.data.completed ? 'completed' : '')}>
+
+                <input type="text"
+                    value={this.state.newText}
+                    onChange={this.handleChange}
+                    onKeyUp={this.isEnter}
+                    onBlur={this.edit} />
+                {/* onClick={this.date}  */}
+                {/* <div className='text' onClick={this.toggleEditMode} className={this.state.editing ? 'hidden' : ''}> {this.props.data.text}</div>
+
+                <input type="text" className={this.state.editing ? '' : 'hidden'} value={this.state.newText} onChange={this.handleChange} onKeyUp={this.isEnter} onBlur={this.edit} /> */}
+
+                <div className='actions'>
+                    <img src="https://www.freeiconspng.com/uploads/check-tick-icon-25.png" alt="Completado" className="check" onClick={this.markAsCompleted} />
+                    <img src="https://www.freeiconspng.com/uploads/garbage-bin-png-8.png" alt="Borrar" className="papelera" onClick={() => this.props.deleteTask(this.props.data.id)} />
+                    {/* <h6 onChange={this.props.data.createAt} /> */}
+                </div>
+            </div>
+        )
+    };
 }
 
 export default Task;
--- src/views/board.jsx
+++ src/views/board.jsx
@@ -1,22 +1,85 @@
-import React, { Component } from 'react';
+import React from 'react';
+import './board.scss';
 import Task from '../components/task.jsx';
 
-import './board.scss';
 
-class Board extends Component {
+class Board extends React.Component {
+    state = {
+        newTaskText: '',
+        tasks: JSON.parse(localStorage.getItem('tasks')) || [],
+    };
+
+    componentDidUpdate() {
+        localStorage.setItem('tasks', JSON.stringify(this.state.tasks))
+    }
+
+
+    addTask = text => {
+        let newTask = {
+            id: Date.now(),
+            createAt: new Date(),
+            text,
+            completed: false,
+        };
+        this.setState({
+            tasks: [newTask, ...this.state.tasks],
+            newTaskText: '',
+            newTaskCreateAt: new Date()
+        });
+    };
+
+    editTask = task => {
+        task = {
+            ...task,
+            updateAt: new Date(),
+        };
+        this.setState({
+            tasks: this.state.tasks.map(_task => (_task.id === task.id ? task : _task)),  //recorre uno a uno todos los elementos del array y los devuelve todos
+        });
+    };
+
+    removeTask = (id) => {
+        this.setState({
+            tasks: this.state.tasks.filter(task => task.id !== id), //recorre los elementos uno a uno y si algun elemento no cumple con la condicion no lo agrega al array y por lo tanto la borra
+        })
+    };
+
+    // dateTask = (createAt) => {
+    //     this.setState({
+    //         newTask: this.state.tasks.createAt(new Date)
+    //     })
+    // }
+
+
+    handleChange = ev => {
+        this.setState({ newTaskText: ev.target.value }); //cambia el dato que se ha creado anteriormente
+        this.setState({ newTaskCreateAt: ev.target.value })
+    };
+
+    handleKeyUp = (ev) => {
+        if (ev.keyCode === 13) {
+            this.addTask(ev.target.value);
+        }
+    };
+
     render() {
         return (
-            <div className="board">
+            <div className='board'>
 
                 <header>
-                    <input type="text" placeholder="add task" />
+                    <input type='text'
+                        placeholder='añade una tarea'
+                        onKeyUp={this.handleKeyUp}
+                        onChange={this.handleChange}
+                        value={this.state.newTaskText}
+                        value={this.state.newTaskCreateAt} />
+                    <button value={this.state.newTaskCreateAt}>Añadir Tarea</button>
                 </header>
 
-                <main className="tasks">
-                    <Task text="aprender react" />
-                    <Task text="aprender jsx" />
-                    <Task text="aprender redux" />
-                    <Task text="aprender angular" />
+                <main className='tasks'>
+                    {this.state.tasks.map(task => (
+                        <Task data={task} deleteTask={this.removeTask} key={task.id} updateTask={this.editTask} addDate={this.newTaskCreateAt} />
+                    ))}
                 </main>
 
                 <footer>
